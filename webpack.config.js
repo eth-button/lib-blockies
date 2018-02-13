10a4dcfd8b3adac81e3a76b375bb0e8cdd738c65
eth-button <eth-button@users.noreply.github.com>
2018-02-13 00:11:53 -0800

    Add eslint, fix entrypoint

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ var package = fs.readFileSync("package.json", "utf-8");
 package = JSON.parse(package);
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /(\.|lib)\/\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
+  },
   entry: "./index.js",
   output: {
     filename: "dist/" + package.name + ".js"
